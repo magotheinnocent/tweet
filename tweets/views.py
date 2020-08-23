71867d64ab8c6de7359625648cf3e1d8d258d156
Innocent  Magothe <innocentmagothe@Innocents-MacBook-Pro.local>
2020-08-23 18:10:01 +0300

    added html file

--- tweets/views.py
+++ tweets/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
+from .models import Tweet
 
 # Create your views here.
 def home_view( request, *args, **kwargs):
-    print (args, kwargs)
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
+
 
 def tweet_detail_view( request,tweet_id, *args, **kwargs):
-    print (args, kwargs)
-    return HttpResponse("<h1>You are wonderful</h1>")
+
+    """
+    REST API VIEW
+    Consumed by all
+    """
+
+    data ={
+        "id": tweet_id,
+        # "image_path": obj.image.url
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content']=obj.content
+    except:
+        data['message'] = "Not Found"
+        status=404
+
+    
+
+    return JsonResponse(data, status=status) #json.dumps content_type=application/json
+
+    #return HttpResponse(f"<h1>You are {tweet_id} - {obj.content}</h1>")
